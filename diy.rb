1c422f4292e05f0039e13b9d09af2b36cb1d98c0
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-10-08 21:49:24 -0600

    Made a stack

--- diy.rb
+++ diy.rb
@@ -1,17 +1,17 @@
 class Stack
     def initialize
-      # create ivar to store stack here!
+        @array = []
     end
 
     def push(el)
-      # adds an element to the stack
+        @array << el
     end
 
     def pop
-      # removes one element from the stack
+        @array.pop
     end
 
     def peek
-      # returns, but doesn't remove, the top element in the stack
+        return @array[-1]
     end
 end
